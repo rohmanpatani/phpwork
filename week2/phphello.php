23deda6ff35cf2b265e47db5759d1ed542f58147
abdulrohman sensor <rohmansensor@gmail.com>
2018-10-30 10:32:37 +0700

    ส่งครั้งที่ 1

---
+++ week2/phphello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello php world</title>
+    </head>
+    <body>
+        <h1>TEST PHP CODE</h1>
+        <?PHP
+            echo "Hello PHP World";
+
+        ?>
+    </body>
+</html>
